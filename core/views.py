e27eceec3fa5e89cc7fefa6e1d76fae0ebd89dce
mielesantos <mielesnts@gmail.com>
2020-02-20 21:36:58 -0300

    adicioando operacao de delete e update de eventos

--- core/views.py
+++ core/views.py
@@ -31,9 +31,16 @@ def submit_login(request):
 
     return redirect("/")
 
+
 @login_required(login_url="/login/")
 def evento(request):
-    return render(request, "evento.html")
+    id_evento = request.GET.get("id")
+
+    dados = {}
+    if id_evento:
+        dados["evento"] = Evento.objects.get(id=id_evento)
+
+    return render(request, "evento.html",dados)
 
 @login_required(login_url="/login/")
 def submit_evento(request):
@@ -43,10 +50,22 @@ def submit_evento(request):
         data_evento = request.POST.get("data_evento")
         descricao = request.POST.get("descricao")
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
-                            data_evento=data_evento,
-                            descricao=descricao,
-                            usuario=usuario)
+        id_evento = request.POST.get("id_evento")
+
+        if id_evento:
+
+            evento = Evento.objects.get(id=id_evento)
+            if usuario == evento.usuario:
+
+                evento.titulo = titulo
+                evento.descricao = descricao 
+                evento.data_evento = data_evento    
+                evento.save()        
+        else:
+            Evento.objects.create(titulo=titulo,
+                                data_evento=data_evento,
+                                descricao=descricao,
+                                usuario=usuario)
         
     return redirect("/")
 
@@ -59,3 +78,13 @@ def lista_eventos(request):
     # evento = Evento.objects.all()
     dados = {"eventos": evento}
     return render(request, "agenda.html", dados)
+
+
+@login_required(login_url="/login/")
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+
+    return redirect("/")
