9b6aeddc7a715acfc471878e4a11e005862923be
mielesantos <mielesnts@gmail.com>
2022-01-10 22:54:05 -0300

    Update settings.py

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2c$*f-vbbsr0$!2&pkc^(dh1vfe^(!3vxk6x$jyegs#p-4k%o='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
